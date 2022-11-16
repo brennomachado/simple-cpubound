18c447607079bfe145abfc994eda4041e6bd545d
Brenno Machado <brenno.pm@gmail.com>
2022-11-15 22:18:26 -0300

    feat; fix; docs; wip

--- cpu-bound.c
+++ cpu-bound.c
@@ -4,9 +4,9 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 
-// Função estresse: Gera estresse de CPU por meio de adição em loop
+// Função estresse 1: Gera estresse de CPU por meio de adição em loop
 //      Parâmentro bilhoes:  inteiro que representa o número de bilhões de iteração que serão realizadas
-int estresse(int bilhoes){
+int estresse1(int bilhoes){
     int i, j, k, l=0;
     for(i=0; i < bilhoes; i++)
         for(j=0; j< 1000; j++)
@@ -15,15 +15,28 @@ int estresse(int bilhoes){
     return 0;
 }
 
+//Função estresse 2: Criar novo método de cálculo, por enquanto é o mesmo o estresse 1 mas com 10bi de interações
+int estresse2(void){
+    int i, j, k, l=0;
+    for(i=0; i < 10000; i++)
+        for(j=0; j< 1000; j++)
+            for(k=0; k< 1000; k++)
+                l++;
+    return 0;
+}
+
+
 // Função main:
 //      Parâmetro 1: inteiro que representa o número de bilhões de iteração que serão realizadas no teste de estresse
 //      Parâmetro 2: inteiro que representa o número de processos que serão abertos para o teste de estresse
 int main(int argc, char *argv[]){
+        double tempo_final;
 
-        if( argc == 3){
-
+        if( argc == 4){
+            int teste = atoi(argv[3]); //escolha do teste a ser executado
             int processos = atoi(argv[2]); // Número de processos que serão abertos
-            int iteracoes = atoi(argv[1]) * 1000; // Número de iterações de adição em bilhões
+            int iteracoes = atoi(argv[1]) * 1000 + 1; // Número de iterações de adição em bilhões
+
             pid_t pid;
             time_t start, end;
 
@@ -38,8 +51,12 @@ int main(int argc, char *argv[]){
                 }
                 else if(pid == 0)
                 {
-                    printf("PROCESSO FILHO N: %d - pid %d - PROCESSO PAI pid: %d\n", i+1, getpid(),getppid()); // Para debug, remover no final
-                    estresse(iteracoes);
+                    // printf("PROCESSO FILHO N: %d - pid %d - PROCESSO PAI pid: %d\n", i+1, getpid(),getppid()); // Para debug, remover no final
+                    
+                    if (teste == 1)
+                        estresse1(iteracoes);
+                    if (teste == 2)
+                        estresse2();
                     exit(0);
                 }
             }
@@ -47,14 +64,16 @@ int main(int argc, char *argv[]){
             if (pid > 0){
                 wait(NULL);
                 time (&end);
-                double tempo_final = difftime (end,start);
-                printf("O programa de estresse levou %f segundos para executar\n", tempo_final); //Para debug, remover no final
-                printf("%f\n", tempo_final);
+                tempo_final = difftime (end,start);
+                // printf("O programa de estresse levou %f segundos para executar\n", tempo_final); //Para debug, remover no final
+                printf("%d\n", (int)tempo_final);
+
+                // printf ("valor do teste %d \n", teste);
             }
         }
         else {
-            printf("ERRO: Não foram inseridos o número de argumentos corretos");
+            printf("ERRO: Não foram inseridos o número de argumentos corretos\n");
         }
-        
-    return 0;
+    // printf("tempo final: %d \n\n", (int)tempo_final);
+    return (0);
 }
